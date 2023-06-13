0214de432b56e28385c94f0e3841788e069ff7c9
sapling-id <5354004+sapling-id@users.noreply.github.com>
2023-06-13 23:28:24 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// builder.Configuration["Kestrel:Certificates:Default:Path"] = "avyan_blue.pem";
+// builder.Configuration["Kestrel:Certificates:Default:KeyPath"] = "privkey.pem";
+builder.WebHost.UseKestrel();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.Urls.Add("http://*:80");
+app.Urls.Add("https://*:443");
+
+string pageToReturn = File.ReadAllText("main.html");
+string styleSheet = File.ReadAllText("style.css");
+byte[] font = File.ReadAllBytes("iAWriterDuospace.woff2");
+byte[] favicon = File.ReadAllBytes("favicon.png");
+
+app.MapGet("/", async context =>
+                {
+                    // await context.Response.WriteAsync(pageToReturn);
+                    context.Response.ContentType = "text/html";
+                    await context.Response.WriteAsync(File.ReadAllText("main.html"));
+                });
+app.MapGet("/font", async context =>
+                {
+                    context.Response.ContentType = "application/font-woff2";
+                    await context.Response.Body.WriteAsync(font);
+                });
+app.MapGet("/favicon", async context =>
+                {
+                    context.Response.ContentType = "image/png";
+                    await context.Response.Body.WriteAsync(favicon);
+                });
+app.MapGet("/style", async context =>
+                {
+                    // await context.Response.WriteAsync(styleSheet);
+                    context.Response.ContentType = "text/css";
+                    await context.Response.WriteAsync(File.ReadAllText("style.css"));
+                });
+
+app.Run();
